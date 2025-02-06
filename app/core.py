b5e9e3eb41bb73af85bc882c491f195525d9aa87
Delvi Fitri Assary <72311126+delvifitri@users.noreply.github.com>
2025-02-06 18:07:58 +0700

    Update core.py

--- app/core.py
+++ app/core.py
@@ -8,14 +8,14 @@ app.secret_key = 'whatever'
 app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get('SQLALCHEMY_DATABASE_URI', 'mysql+pymysql://root@localhost:3306/fd_reviews')
 db = SQLAlchemy(app)
 
-def set_sql_mode(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))")
-    cursor.close()
+#def set_sql_mode(dbapi_connection, connection_record):
+#    cursor = dbapi_connection.cursor()
+#    cursor.execute("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))")
+#    cursor.close()
 
-with app.app_context():
-    engine = db.get_engine()
-    event.listen(engine, 'connect', set_sql_mode)
+#with app.app_context():
+#    engine = db.get_engine()
+#    event.listen(engine, 'connect', set_sql_mode)
 
 from app.views.index import index
 from app.views.admin import admin
